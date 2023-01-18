3ff5ee6a188f53f1b63fd47de121b8b01851fe16
Tabbie254 <tabbie440@gmail.com>
2023-01-18 15:13:30 +0300

    successfully connected to mongodb

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ let index = require('./routes/index');
 let image = require('./routes/image');
 
 // connecting the database
-let mongodb_url = 'mongodb://localhost:27017/';
+let mongodb_url = 'mongodb://127.0.0.1:27017/';
 let dbName = 'darkroom';
 mongoose.connect(`${mongodb_url}${dbName}`,{ useNewUrlParser: true , useUnifiedTopology: true }, (err)=>{
     if (err) console.log(err)
@@ -40,7 +40,7 @@ app.use('/image', image);
 
 
  
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5050;
 app.listen(PORT,() =>{
-    console.log(`Server is listening at http://localhost:${PORT}`)
+    console.log(`Server is listening at http://127.0.0.1:${PORT}`)
 });
