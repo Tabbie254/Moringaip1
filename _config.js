c626b132c64049437a652e8c2a55252adefd2779
Tabbie254 <tabbie440@gmail.com>
2023-01-18 16:10:58 +0300

    more changes

--- _config.js
+++ _config.js
@@ -2,20 +2,8 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-<<<<<<< HEAD
-<<<<<<< HEAD
-    production: 'mongodb+srv://Tabbie254ke:tapamuts2021@cluster0.5ljtko7.mongodb.net/test',
-    development: 'mongodb+srv://Tabbie254ke:tapamuts2021@cluster0.5ljtko7.mongodb.net/test',
-    test: 'mongodb+srv://Tabbie254ke:tapamuts2021@cluster0.5ljtko7.mongodb.net/test',
-=======
-    production: 'mongodb+srv://Tabbie254ke:tapamuts2021@cluster0.5ljtko7.mongodb.net/sample_mflix?retryWrites=true&w=majority',
-    development: 'mongodb+srv://Tabbie254ke:tapamuts2021@cluster0.5ljtko7.mongodb.net/sample_mflix?retryWrites=true&w=majority',
-    test: 'mongodb+srv://Tabbie254ke:tapamuts2021@cluster0.5ljtko7.mongodb.net/sample_mflix?retryWrites=true&w=majority',
->>>>>>> 4d58d49d21f4d55848da563adceb5bdf9d40c947
-=======
-    production: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
->>>>>>> test
+    production:'mongodb+srv://Tabbie254ke:tapamuts2021@cluster0.5ljtko7.mongodb.net/test',
+    development:'mongodb+srv://Tabbie254ke:tapamuts2021@cluster0.5ljtko7.mongodb.net/test',
+    test:'mongodb+srv://Tabbie254ke:tapamuts2021@cluster0.5ljtko7.mongodb.net/test',
 }
 module.exports = config;
--- server.js
+++ server.js
@@ -2,44 +2,26 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const path = require('path');
-const config = require('./_config');
 
 // Define routes
 let index = require('./routes/index');
 let image = require('./routes/image');
 
-// Initializing the app
-const app = express();
-
 // connecting the database
-<<<<<<< HEAD
-<<<<<<< HEAD
 let mongodb_url = 'mongodb://127.0.0.1:27017/';
-=======
-let mongodb_url = 'mongodb://localhost/';
->>>>>>> 4d58d49d21f4d55848da563adceb5bdf9d40c947
 let dbName = 'darkroom';
 mongoose.connect(`${mongodb_url}${dbName}`,{ useNewUrlParser: true , useUnifiedTopology: true }, (err)=>{
     if (err) console.log(err)
-=======
-
-const MONGODB_URI = process.env.MONGODB_URI || config.mongoURI[app.settings.env]
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true  },(err)=>{
-    if (err) {
-        console.log(err)
-    }else{
-        console.log(`Connected to Database: ${MONGODB_URI}`)
-    }
->>>>>>> test
 });
 
 // test if the database has connected successfully
-// let db = mongoose.connection;
-// db.once('open', ()=>{
-//     console.log('Database connected successfully')
-// })
+let db = mongoose.connection;
+db.once('open', ()=>{
+    console.log('Database connected successfully')
+})
 
-
+// Initializing the app
+const app = express();
 
 
 // View Engine
@@ -60,13 +42,5 @@ app.use('/image', image);
  
 const PORT = process.env.PORT || 5050;
 app.listen(PORT,() =>{
-<<<<<<< HEAD
     console.log(`Server is listening at http://127.0.0.1:${PORT}`)
 });
-=======
-    console.log(`Server is listening at http://localhost:${PORT}`)
-});
-
-
-module.exports = app;
->>>>>>> test
